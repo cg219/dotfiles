a1efb88d7322fabde7f37d7d25871e2ef326ab1f
Clemente <dev@imkreative.com>
2020-08-29 02:22:49 -0400

    Disable GK

--- setup/defaults-osx.sh
+++ setup/defaults-osx.sh
@@ -1,5 +1,6 @@
 echo "Setup defaults..."
 
+sudo spctl --master-disable
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 0
 defaults write com.apple.keyboard.fnState -int 1
