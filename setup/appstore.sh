151588ffa57ff3bb5a32647801a9f90db9b22085
Clemente <dev@imkreative.com>
2021-12-06 13:06:31 -0500

    Update NordVPN version

--- setup/appstore.sh
+++ setup/appstore.sh
@@ -1,4 +1,4 @@
 read -p "Hit [Enter] after you sign in to the App Store..."
 echo "Install App Store apps..."
-mas install 1116599239 # NordVPN
+mas install 905953485 # NordVPN
 mas install 497799835 # Xcode
