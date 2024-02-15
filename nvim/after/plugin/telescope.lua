34e67c4530c2021514ef6f27f62324ee02c5d5d6
Mente <>
2024-02-14 19:58:09 -0500

    neovim config updates

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
+
 nfmap('<leader>ff', builtin.find_files)
 nfmap('<leader>pp', builtin.git_files)
 nfmap('<leader>ss', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")})
 end)
+
