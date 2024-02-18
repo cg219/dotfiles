7621d68845bc5da5c9b0b4b6467925448f069cb4
Mente <dev@imkreative.com>
2024-02-18 12:02:49 -0500

    Markdown setup Option Key iTerm2 config

--- nvim/lua/mentegee/packer.lua
+++ nvim/lua/mentegee/packer.lua
@@ -75,6 +75,14 @@ return require('packer').startup(function(use)
       config = true
   }
 
+  -- install without yarn or npm
+  use {
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn["mkdp#util#install"]() end,
+  }
+
+  use { "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }
+
   if packer_bootstrap then
     require("packer").sync();
   end
