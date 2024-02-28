10eba0af2dfb24db54ff3dee2f47df743ff162a4
Mente <dev@imkreative.com>
2024-02-28 04:18:42 -0500

    save remap

--- nvim/lua/mentegee/remap.lua
+++ nvim/lua/mentegee/remap.lua
@@ -11,10 +11,11 @@ nmap("<C-u>", "<C-u>zz")
 nmap("<leader>u", ":UndotreeToggle<cr>")
 nmap("<leader>ut", ":Undotree<cr>")
 nmap("<leader>gi", ":LazyGit<cr>")
+nmap("<leader>s", ":w<cr>")
 
 nfmap('<leader>ff', builtin.find_files)
 nfmap('<leader>pp', builtin.git_files)
-nfmap('<leader>ss', function()
+nfmap('<leader>gs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")})
 end)
 
