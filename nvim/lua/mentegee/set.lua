663c28eed7c7a8749b3f20306e35ceb9e526598f
Mente <>
2024-03-03 21:45:12 -0500

    Update telescope settings

--- nvim/lua/mentegee/set.lua
+++ nvim/lua/mentegee/set.lua
@@ -1,5 +1,5 @@
 vim.opt.guicursor = ""
-vim.opt.clipboard = "unnamed"
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -31,3 +31,17 @@ vim.opt.colorcolumn = "120"
 vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
