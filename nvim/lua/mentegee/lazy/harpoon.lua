130db34b33669bc9943f11c75daa9e6a9b43d72e
Mente <dev@imkreative.com>
2024-04-15 01:30:57 -0400

    Update configs
    - Update Harpoon configfor quick menu and telescope
    - add home folder to findproject

--- nvim/lua/mentegee/lazy/harpoon.lua
+++ nvim/lua/mentegee/lazy/harpoon.lua
@@ -4,12 +4,32 @@ return {
     dependencies = {"nvim-lua/plenary.nvim"},
     config = function ()
         local harpoon = require("harpoon")
+
         -- REQUIRED
         harpoon:setup()
         -- REQUIRED
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        -- basic telescope configuration
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
+        end
+
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>e", function() harpoon.ui.toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>E", function() toggle_telescope(harpoon:list()) end)
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
