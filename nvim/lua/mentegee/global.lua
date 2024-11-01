b8128b07394fb3cfd12710052b7baafecedae60e
Mente <dev@imkreative.com>
2024-11-01 03:17:39 -0400

    update for stow
    - add freeze to nvim
    - configure
    - add kitty.conf

--- nvim/lua/mentegee/global.lua
+++
@@ -1,4 +0,0 @@
-nmap = function(map, cmd) vim.api.nvim_set_keymap("n", map, cmd, { noremap = true, silent = true }) end
--- vmap = function(map, cmd) vim.api.nvim_set_keymap("v", map, cmd, { noremap = true, silent = true }) end
-nfmap = function(map, cmd) vim.keymap.set("n", map, cmd, { noremap = true, silent = true }) end
--- vfmap = function(map, cmd) vim.keymap.set("v", map, cmd, { noremap = true, silent = true }) end
