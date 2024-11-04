0ed22826d6adabda61cc9907352cba8cd6c33973
Mente <dev@imkreative.com>
2024-11-04 14:43:50 -0500

    update

--- nix/flake.nix
+++ nix/flake.nix
@@ -17,117 +17,54 @@
         brew-charm.flake = false;
         brew-ethereum.url = "github:ethereum/homebrew-ethereum";
         brew-ethereum.flake = false;
+        brew-turso.url = "github:tursodatabase/homebrew-tap";
+        brew-turso.flake = false;
     };
 
-    outputs = inputs@{ self, darwin, nixpkgs, nix-homebrew, brew-core, brew-cask, brew-charm, brew-ethereum, home-manager }:
+    outputs = inputs@{ self, darwin, nixpkgs, nix-homebrew, brew-turso, brew-core, brew-cask, brew-charm, brew-ethereum, home-manager }:
         let configuration = { pkgs, config, ... }: {
             nixpkgs.config.allowUnfree = true;
             # List packages installed in system profile. To search by name, run:
             # $ nix-env -qaP | grep wget
-            environment.systemPackages = [
-                pkgs.neovim
-                pkgs.git-extras
-                pkgs.gitflow
-                pkgs.tree
-                pkgs.wget
-                pkgs.mas
-                pkgs.fnm
-                pkgs.go
-                pkgs.lua
-                pkgs.lazygit
-                pkgs.libfido2
-                pkgs.turso-cli
-                pkgs.kitty
-                pkgs.android-tools
-                pkgs.bruno
-                pkgs.cron
-                pkgs.tableplus
-                pkgs.discord
-                pkgs.docker
-                pkgs.obsidian
-                pkgs.slack
-                pkgs.spotify
-                pkgs.mpv
-                pkgs.mkalias
-                pkgs.tmux
-                pkgs.docker-compose
-            ];
+            environment.systemPackages = [];
 
             homebrew = {
-                enable = true;
-                # taps = {
-                #     "homebrew/homebrew-core" = brew-core;
-                #     "homebrew/homebrew-cask" = brew-cask;
-                #     "charmbracelet/tap" = brew-charm;
-                #     "ethereum/ethereum" = brew-ethereum;
-                # };
+                enable = false;
                 brews = [
-                    "charmbracelet/tap/charm"
-                    "charmbracelet/tap/freeze"
-                    "charmbracelet/tap/skate"
-                    "grep"
-                    "trash"
+                    "tursodatabase/tap/turso"
                 ];
-                # casks = [
-                #     "affinity-designer"
-                #     "affinity-photo"
-                #     "affinity-publisher"
-                #     "alfred"
-                #     "arc"
-                #     "balenaetcher"
-                #     "firefox"
-                #     "sublime-text"
-                #     "sublime-merge"
-                #     "ledger-live"
-                #     "dropbox"
-                #     "rightfont"
-                #     "sf-symbols"
-                #     "skype"
-                #     "font-ubuntu-mono-nerd-font"
-                #     "vlc"
-                # ];
+                casks = [
+                    "affinity-designer"
+                    "affinity-photo"
+                    "affinity-publisher"
+                    "alfred"
+                    "arc"
+                    "balenaetcher"
+                    "firefox"
+                    "sublime-text"
+                    "sublime-merge"
+                    "ledger-live"
+                    "dropbox"
+                    "rightfont"
+                    "sf-symbols"
+                    "skype"
+                    "font-ubuntu-mono-nerd-font"
+                    "vlc"
+                    "utm"
+                ];
                 onActivation.autoUpdate = true;
                 onActivation.upgrade = true;
+                onActivation.cleanup = "uninstall";
             };
 
             # Auto upgrade nix package and the daemon service.
             services.nix-daemon.enable = true;
-            # nix.package = pkgs.nix;
+            nix.package = pkgs.nix;
 
             # Necessary for using flakes on this system.
             nix.settings.experimental-features = ["nix-command" "flakes"];
 
             programs.zsh.enable = true;  # default shell on catalina
-
-            # programs.zsh = {
-            #     enable = true;
-            #     shellAliases = {
-            #         switch = "darwin-rebuild switch --flake ~/dotfiles/nix/#dev-macOS";
-            #         mente = "cd ~";
-            #         desk = "cd ~/Desktop";
-            #         web = "cd ~/websites";
-            #         dev = "cd ~/development";
-            #         apps = "cd ~/apps";
-            #         vim = "nvim";
-            #         n = "nvim .";
-            #         gateo = "sudo spctl --master-disable";
-            #         gatec = "sudo spctl --master-enable";
-            #         getssh = "cat ~/.ssh/id_rsa.pub | pbcopy";
-            #         ".." = "cd ../";
-            #         "..." = "cd ../../";
-            #         gin = "git init";
-            #         gad = "git add";
-            #         gomit = "git commit -m";
-            #         gush = "git push";
-            #         gull = "git pull";
-            #         gsus = "git status";
-            #         today = "git log --pretty=\"· %s\" --author='Clemente' --since=\"5am\" --no-merges --all";
-            #         yesterday = "git log --pretty=\"· %s\" --author='Clemente' --since=\"yesterday\" --no-merges --all";
-            #         obsidian = "$HOME/Library/Mobile\ Documents/iCloud~md~obsidian/Documents && n";
-            #         f = "findproject";
-            #         fs = "fuzzysession";
-            #     };
-            # };
 
             # programs.tmux.enable = true;
             # programs.tumx.enableFzf = true;
@@ -171,14 +108,83 @@
                 '';
 
         };
-        mentegee = {pkgs, config, ... }: {
+            mentegee = {pkgs, config, ... }: {
+                nixpkgs.config.allowUnfree = true;
                 home.packages = [
+                    pkgs.neovim
+                    pkgs.git-extras
+                    pkgs.gitflow
+                    pkgs.tree
+                    pkgs.wget
+                    pkgs.mas
+                    pkgs.fnm
+                    pkgs.go
+                    pkgs.lua
+                    pkgs.lazygit
+                    pkgs.libfido2
+                    pkgs.turso-cli
+                    pkgs.kitty
+                    pkgs.android-tools
+                    pkgs.bruno
+                    pkgs.cron
+                    pkgs.tableplus
+                    pkgs.discord
+                    pkgs.docker
+                    pkgs.obsidian
+                    pkgs.slack
+                    pkgs.spotify
+                    pkgs.mpv
+                    pkgs.mkalias
+                    pkgs.tmux
+                    pkgs.docker-compose
+                    pkgs.charm-freeze
+                    pkgs.skate
+                    pkgs.charm
+                    pkgs.gnugrep
+                    # pkgs.trashy
                 ];
                 programs.home-manager.enable = true;
+                programs.fzf = {
+                    enable = true;
+                    tmux = {
+                        enableShellIntegration = true;
+                    };
+                    enableZshIntegration = true;
+                };
+
+                programs.zsh = {
+                    enable = true;
+                    shellAliases = {
+                        switch = "darwin-rebuild switch --flake ~/dotfiles/nix/#dev-macOS";
+                        mente = "cd ~";
+                        desk = "cd ~/Desktop";
+                        web = "cd ~/websites";
+                        dev = "cd ~/development";
+                        apps = "cd ~/apps";
+                        vim = "nvim";
+                        n = "nvim .";
+                        gateo = "sudo spctl --master-disable";
+                        gatec = "sudo spctl --master-enable";
+                        getssh = "cat ~/.ssh/id_rsa.pub | pbcopy";
+                        ".." = "cd ../";
+                        "..." = "cd ../../";
+                        gin = "git init";
+                        gad = "git add";
+                        gomit = "git commit -m";
+                        gush = "git push";
+                        gull = "git pull";
+                        gsus = "git status";
+                        today = "git log --pretty=\"· %s\" --author='Clemente' --since=\"5am\" --no-merges --all";
+                        yesterday = "git log --pretty=\"· %s\" --author='Clemente' --since=\"yesterday\" --no-merges --all";
+                        obsidian = "$HOME/Library/Mobile\ Documents/iCloud~md~obsidian/Documents && n";
+                        f = "findproject";
+                        fs = "fuzzysession";
+                    };
+                };
 
                 home.stateVersion = "24.05";
 
-                home.file.".zshrc".source = ./../.zshrc;
+                home.file.".zshrc2".source = ./../.zshrc;
                 home.file.".zshenv".source = ./../.zshenv;
                 home.file.".tmux.conf".source = ./../.tmux.conf;
                 home.file.".config/freeze" = {
@@ -197,7 +203,7 @@
                     source = config.lib.file.mkOutOfStoreSymlink ./../.config/nvim;
                     recursive =  true;
                 };
-        };
+            };
         in
             {
             # Build darwin flake using:
@@ -207,11 +213,18 @@
                     configuration
                     nix-homebrew.darwinModules.nix-homebrew {
                         nix-homebrew = {
-                            enable = true;
+                            enable = false;
                             enableRosetta = true;
                             user = "mentegee";
-                            autoMigrate = true;
-                            # mutableTaps = false;
+                            # autoMigrate = true;
+                            mutableTaps = false;
+
+                            taps = {
+                                "homebrew/homebrew-core" = brew-core;
+                                "homebrew/homebrew-cask" = brew-cask;
+                                "charmbracelet/tap" = brew-charm;
+                                "ethereum/ethereum" = brew-ethereum;
+                            };
                         };
                     }
                     home-manager.darwinModules.home-manager {
