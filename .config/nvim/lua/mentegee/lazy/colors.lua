99f5b5e76acbe5870f3759c7401bf9e38df7aec0
Mente Gee <dev@imkreative.com>
2025-08-09 10:22:37 -0400

    update to catppuccin-macchiato nvim theme

--- .config/nvim/lua/mentegee/lazy/colors.lua
+++ .config/nvim/lua/mentegee/lazy/colors.lua
@@ -1,14 +1,14 @@
 return {
-    "folke/tokyonight.nvim",
+    -- "folke/tokyonight.nvim",
+    "catppuccin/nvim",
     {
         "Mofiqul/dracula.nvim",
         config = function ()
-            vim.cmd[[colorscheme dracula]]
+            vim.cmd[[colorscheme catppuccin-macchiato]]
+            -- vim.cmd[[colorscheme dracula]]
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
-
         end
-
     }
 }
 
